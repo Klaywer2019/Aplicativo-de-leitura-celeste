5ecb440d6f4cbb94a8dbad6a456a8d0c81b91a4e
Klaywer2019 <keisejemima@gmail.com>
2026-05-27 22:46:33 -0300

    Criar capitulo2.js

---
+++ capitulo2.js
@@ -0,0 +1,5 @@
+const textoCapitulo1 = [
+    "A presença da radiação gama era tão sufocante que comecei a achar...",
+    "De quem são essas vozes? Eram vozes distorcidas...",
+    "— Você tá, chorando? Isso é raro — falou ela..."
+];
